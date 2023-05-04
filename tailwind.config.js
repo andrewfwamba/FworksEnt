d9c107b0a3987536eb215ab7d8a281b6d1abfac0
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-05-04 23:02:10 +0300

    tailwind configs

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        black: "#09090c",
+        darkGray: "#121212",
+
+        brightRed: "hsl(12, 88%, 59%)",
+        brightRedLight: "hsl(12, 88%, 69%)",
+        brightRedSupLight: "hsl(12, 88%, 95%)",
+
+        darkBlue: "hsl(228, 39%, 23%)",
+        darkGrayishBlue: "hsl(227, 12%, 61%)",
+        veryDarkBlue: "hsl(233, 12%, 13%)",
+      },
+    },
+  },
+  plugins: [],
+};
