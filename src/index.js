4eefad806ff4d50678e206357e10f9019e4ff0e9
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-05-04 23:04:21 +0300

    theme

--- src/index.js
+++ src/index.js
@@ -19,7 +19,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <Background>
+        <App />
+      </Background>
     </ThemeProvider>
   </React.StrictMode>
 );
