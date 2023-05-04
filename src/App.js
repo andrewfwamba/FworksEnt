c99da74b587d0195320b6f75f9895ee3fc28bbe5
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-05-04 23:04:54 +0300

    initial routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Layout from "./components/Layout";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Discover from "./routes/Discover";
+
+// import Home from "./pages/Home";
+// import Dashboard from "./pages/Dashboard";
+// import Course from "./pages/Course";
+// import Profile from "./pages/Profile";
+// import Login from "./pages/Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/discover" element={<Discover />} />
+          {/* <Route path="/course" element={<Course />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/login" element={<Login />} /> */}
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
