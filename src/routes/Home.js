e76e49f8be5a67855ecfc025f319be689377d203
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-05-04 23:09:14 +0300

    template for home

---
+++ src/routes/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="font-bold text-center py-20 dark:text-white">
+      <h3>Home</h3>
+    </div>
+  );
+}
+
+export default Home;
