1d5126613a9017cc1fda1c85c814e45a98286c52
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-05-05 22:57:50 +0300

    placeholder home

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Home() {
   return (
-    <div className="font-bold text-center py-20 dark:text-white">
+    <div className="w-full h-screen bg-teal-200 text-center  dark:text-white">
       <h3>Home</h3>
     </div>
   );
