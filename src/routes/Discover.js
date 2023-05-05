0fae3b515c709958660ca35bff88362d84990afe
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-05-05 22:57:22 +0300

    placeholder dicover

--- src/routes/Discover.js
+++ src/routes/Discover.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Discover() {
-  return <div className="w-full h-full p-96 bg-gray-400">Discover</div>;
+  return (
+    <div className="w-full h-screen justify-center items-center bg-gray-200 text-center">
+      Discover
+    </div>
+  );
 }
 
 export default Discover;
