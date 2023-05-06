fca5c0fb4816170bb017b8856e0bfd8137a23dd3
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-05-06 18:35:59 +0300

    integrate outlet

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,17 +2,13 @@ import React from "react";
 
 function Nav() {
   return (
-    <nav className="bg-white border-gray-200 px-2 py-2.5 dark:bg-gray-800">
+    <nav className="bg-white border-gray-200 px-2 p-1.5 md:py-2.5 dark:bg-gray-800">
       <div className="container flex justify-between items-center mx-auto pt-3">
         <div className="flex items-center mx-auto">
-          <span className="text-xl font-medium whitespace-nowrap dark:text-white">
+          <span className="text-lg font-medium whitespace-nowrap dark:text-white">
             Welcome, Andrew Fwamba
           </span>
         </div>
-
-        {/* <div className='flex justify-end pr-4'>
-        <Toggle />
-      </div> */}
       </div>
     </nav>
   );
