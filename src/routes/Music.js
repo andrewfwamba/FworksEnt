843be731cd3b8dc7e3eedb2202fcd5961034dc56
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-05-06 18:36:47 +0300

    music page temp

---
+++ src/routes/Music.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Music() {
+  return (
+    <div className="w-full h-screen dark:bg-gray-600 text-center  dark:text-white">
+      Music
+    </div>
+  );
+}
+
+export default Music;
