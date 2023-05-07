a7741fb25fa907c4c7ab8c91974e33c9e923e9bc
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-05-07 19:07:06 +0300

    frames

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,7 +3,12 @@ import React from "react";
 function Home() {
   return (
     <div className="w-full h-screen bg-teal-200 text-center  dark:text-white">
-      <h3>Home</h3>
+      <div className="grid sm:grid-cols-2 md:grid-cols-3 pt-4 px-2 w-full gap-2">
+        <div className="md:col-span-2 p-20 bg-slate-400 rounded"></div>
+        <div className="bg-white p-20 rounded md:row-span-2 shadow-md"></div>
+        <div className="sm:col-span-2 p-20 bg-slate-400 rounded-sm shadow"></div>
+      </div>
+      <div className="p-20 bg-white my-4 mx-2"></div>
     </div>
   );
 }
