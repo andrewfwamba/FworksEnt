4ec7e5eb3dd7c6231aeef8ee4df00e630f0e965b
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-05-11 19:26:19 +0300

    additional api call routes

--- src/assets/api.js
+++ src/assets/api.js
@@ -2,5 +2,8 @@ const baseURL = "http://192.168.43.187:6060";
 
 const health = `${baseURL}/ping`;
 const getMusic = `${baseURL}/api/v1/music/get/`;
+const signUp = `${baseURL}/api/v1/users/create`;
+const login = `${baseURL}/api/v1/users/signin`;
+const stream = `${baseURL}/api/v1/music/mp3/`;
 
-export { health, getMusic };
+export { baseURL, health, signUp, login, getMusic, stream };
