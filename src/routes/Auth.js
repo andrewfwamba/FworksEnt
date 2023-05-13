5897bc533b1ac40b40a824eb198348dc0c70fcda
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-05-13 23:55:29 +0300

    login component

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,22 +1,26 @@
 import React from "react";
 import Header from "../components/Heading";
+import Login from "../components/Login";
 
-function Signup() {
+function LoginPage() {
   return (
-    <div className="w-full h-screen bg-slate-300">
-      <nav className="h-14 px-10 py-4 bg-slate-900">
-        <h3 className="dark:text-gray-100">Fireworks Entertainment</h3>
-      </nav>
-      <div className="mt-10">
-        <Header
-          heading="Login to your account"
-          paragraph="Don't have an account yet? "
-          linkName="Signup"
-          linkUrl="/signup"
-        />
+    <>
+      <div className="w-full h-screen bg-slate-300">
+        <nav className="h-14 px-10 py-4 bg-slate-900">
+          <h3 className="dark:text-gray-100">Fireworks Entertainment</h3>
+        </nav>
+        <div className="mt-10">
+          <Header
+            heading="Login to your account"
+            paragraph="Don't have an account yet? "
+            linkName="Signup"
+            linkUrl="/signup"
+          />
+          <Login />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
-export default Signup;
+export default LoginPage;
