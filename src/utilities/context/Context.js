c9e96f1e8d4f489ae45e1c68af8b5f65f5291d4d
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-06-10 20:09:23 +0300

    update

---
+++ src/utilities/context/Context.js
@@ -0,0 +1,44 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const LoginContext = createContext();
+
+const LoginProvider = ({ children }) => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [user, setUser] = useState(null);
+  const [processing, setProcessing] = useState(true);
+
+  const fetchUser = async () => {
+    setProcessing(true);
+    const userData = localStorage.getItem("user");
+    let user = JSON.parse(userData);
+    setUser(user);
+    if (userData !== null) {
+      setLoggedIn(true);
+      setTimeout(() => {
+        setProcessing(false);
+      }, 2000);
+    } else {
+      setLoggedIn(false);
+      setProcessing(false);
+    }
+  };
+  useEffect(() => {
+    fetchUser();
+  }, [loggedIn]);
+  return (
+    <LoginContext.Provider
+      value={{
+        loggedIn,
+        setLoggedIn,
+        user,
+        setUser,
+        processing,
+        setProcessing,
+      }}
+    >
+      {children}
+    </LoginContext.Provider>
+  );
+};
+export const useLogin = () => useContext(LoginContext);
+export default LoginProvider;
